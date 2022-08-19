1a8527eec41ea9d09d534b1aaff5e23ab94f3744
Klemen Vodopivec <klemen@vodopivec.org>
2022-08-19 16:55:25 -0400

    Add support for adding/removing acronyms, partial support for updating

--- acronyms.js
+++ acronyms.js
@@ -2,9 +2,11 @@ const acronymCardTemplate = document.querySelector("[data-acronym-template]")
 const acronymCardContainer = document.querySelector("[data-acronym-cards-container]")
 const searchInput = document.querySelector("[data-search]")
 
-const URL = "?limit=100&search="
-
-let acronyms = []
+const ADD_URL = "?add=";
+const EDIT_URL = "?change=";
+const REMOVE_URL = "?remove=";
+const SEARCH_URL = "?limit=100&search="
+const TAGS_URL = "?tags="
 
 function triggerSearch(value) {
   searchInput.value = value;
@@ -12,64 +14,219 @@ function triggerSearch(value) {
   searchInput.dispatchEvent(trigger);
 }
 
-function fetchAndShow(url) {
-  fetch(url)
+function refreshTags() {
+  fetch(TAGS_URL)
     .then(res => res.json())
     .then(data => {
-      data.forEach(acronym => {
-        const card = acronymCardTemplate.content.cloneNode(true).children[0]
-        card.querySelector("[data-name]").textContent = acronym.name
-        card.querySelector("[data-definition]").textContent = acronym.definition
+      EditCardHandler.assignTags(data);
+    });
+}
 
-        if (acronym.description) {
-          card.querySelector("[data-description]").textContent = acronym.description
-        } else {
-          card.querySelector("[data-description]").classList.add("hide")
-        }
+class AcronymCardHandler {
+  /** Assign JSON data to HTML card container */
+  static assign(card, data) {
 
-        if (acronym.tags) {
-          acronym.tags.forEach(tag => {
-            const el = card.querySelector("[data-acronym-tag-template]").content.cloneNode(true).children[0]
-            console.log(el)
-            el.textContent = tag
-            el.href = "javascript: triggerSearch('" + tag + "');";
-            card.querySelector("[data-tags]").append(el)
-          })
-        } else {
-          card.querySelector("[data-tags]").classList.add("hide")
-        }
+    // Acronym name and long definition are required fields and always available
+    card.querySelector("[data-id]").textContent = data.id
+    card.querySelector("[data-name]").textContent = data.name
+    card.querySelector("[data-definition]").textContent = data.definition
 
-        if (acronym.link) {
-          card.querySelector("[data-link]").textContent = acronym.link.replace(/(.{100})..+/, "$1...")
-          card.querySelector("[data-link]").href = acronym.link
-        } else {
-          card.querySelector("[data-link]").style.display = "none";
-        }
+    // Don't hide description to keep the action icons visible
+    card.querySelector("[data-description]").textContent = data.description;
 
-        card.querySelector("[data-expanded]").style.display = "none";
-        card.addEventListener("click", function() {
-          const content = this.querySelector("[data-expanded]");
-          if (content.style.display === "block") {
-            content.style.display = "none";
-            this.classList.remove("active");
-          } else {
-            content.style.display = "block";
-            this.classList.add("active");
+    // Hide external link if empty, but also display long links shorter
+    if (data.link) {
+      card.querySelector("[data-link]").textContent = data.link.replace(/(.{100})..+/, "$1...")
+      card.querySelector("[data-link]").href = data.link
+    } else {
+      card.querySelector("[data-link]").style.display = "none";
+    }
+
+    // Instantiate a new div object for each tag for a nicer effect
+    if (data.tags) {
+      data.tags.forEach(tag => {
+        const el = card.querySelector("[data-acronym-tag-template]").content.cloneNode(true).children[0]
+        el.textContent = tag
+        el.href = "javascript: triggerSearch('" + tag + "');";
+        card.querySelector("[data-tags]").append(el)
+      })
+    } else {
+      card.querySelector("[data-tags]").style.display = "none";
+    }
+
+    // Change the fields to editable when Edit button is pressed
+    card.querySelector("[data-edit]").addEventListener("click", function () {
+      // No need to expand the card, its own event listener already did it
+      //AcronymCardHandler.enableEdits(card);
+      console.log("Want to edit this card?");
+    });
+
+    card.querySelector("[data-delete]").addEventListener("click", function () {
+      const id = card.querySelector("[data-id]").textContent;
+      fetch(REMOVE_URL + id)
+        .then(res => res.json())
+        .then(data => {
+          if (data.status == "ok") {
+            acronymCardContainer.removeChild(card);
           }
         });
+    });
 
-        acronymCardContainer.append(card)
-      })
-    })
+    // Expand or collapse the acronym details, like tags, description etc.
+    card.querySelector("[data-expanded]").style.display = "none";
+    card.querySelector("[data-actions]").style.display = "none";
+    card.addEventListener("click", function (event) {
+      // Ignore event for action icons
+      const skip = ["[data-delete]", "[data-edit]"];
+      for (const el of skip) {
+        if (event.target == card.querySelector(el)) {
+          return;
+        }
+      }
+
+      const content = card.querySelector("[data-expanded]");
+      const actions = card.querySelector("[data-actions]");
+      if (content.style.display === "block") {
+        content.style.display = "none";
+        actions.style.display = "none";
+        card.classList.remove("active");
+      } else {
+        content.style.display = "block";
+        actions.style.display = "block";
+        card.classList.add("active");
+      }
+    });
+
+  }
+
+  static enableEdits(card) {
+    const nameInput = document.createElement("INPUT");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("value", card.querySelector("[data-name]").textContent);
+    nameInput.setAttribute("placeholder", "ACRONYM");
+    card.querySelector("[data-name]").textContent = "";
+    card.querySelector("[data-name]").appendChild(nameInput);
+  }
 }
+
+class EditCardHandler {
+  static init() {
+    const editCard = document.querySelector("[data-card-edit]");
+
+    editCard.style.display = "none";
+    editCard.querySelector("[data-confirm]").addEventListener("click", EditCardHandler.submit);
+    editCard.querySelector("[data-cancel]").addEventListener("click", EditCardHandler.cancel);
+  }
+
+  static assignTags(tags) {
+    const editCard = document.querySelector("[data-card-edit]");
+    // Add support for autocompleting the fields
+  }
+
+  static submit() {
+    const editCard = document.querySelector("[data-card-edit]");
+
+    const acronym = Object();
+    acronym.name = editCard.querySelector("[data-name]").value;
+    acronym.definition = editCard.querySelector("[data-definition]").value;
+    if (acronym.name && acronym.definition) {
+      if (editCard.querySelector("[data-description]").value) {
+        acronym.description = editCard.querySelector("[data-description]").value;
+      }
+      if (editCard.querySelector("[data-link]").value) {
+        acronym.link = editCard.querySelector("[data-link]").value;
+        if (acronym.link.search(/^http[s]?\:\/\//) == -1) {
+          acronym.link = "http://" + acronym.link;
+        }
+      }
+      const tags = [];
+      ["[data-tag1]", "[data-tag2]", "[data-tag3]", "[data-tag4]", "[data-tag5]"].forEach(el => {
+        const tag = editCard.querySelector(el).value;
+        if (tag) {
+          tags.push(tag);
+        }
+      });
+      if (tags) {
+        acronym.tags = tags;
+      }
+
+      fetch(ADD_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(acronym),
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if (data.status == "ok") {
+            EditCardHandler.hide();
+            EditCardHandler.clear();
+
+            const card = acronymCardTemplate.content.cloneNode(true).children[0];
+            AcronymCardHandler.assign(card, acronym);
+            acronymCardContainer.prepend(card);
+            refreshTags();
+          }
+        });
+    }
+  }
+
+  static cancel() {
+    const editCard = document.querySelector("[data-card-edit]");
+
+    // Invalidate the id to prevent accidental submittal
+    editCard.querySelector("[data-id]").value = "";
+    editCard.style.display = "none";
+  }
+
+  static toggle(id = "") {
+    const editCard = document.querySelector("[data-card-edit]");
+
+    if (editCard.style.display != "none") {
+      editCard.style.display = "none";
+    } else {
+      editCard.style.display = "block";
+
+      if (!id) {
+        acronymCardContainer.prepend(editCard);
+      }
+    }
+  }
+
+  static hide() {
+    const editCard = document.querySelector("[data-card-edit]");
+
+    editCard.style.display = "none";
+    document.body.append(editCard);
+  }
+
+  static clear() {
+    const editCard = document.querySelector("[data-card-edit]");
+    const fields = [ "[data-id]", "[data-name]", "[data-definition]",
+                     "[data-description]", "[data-link]", "[data-tag1]",
+                     "[data-tag2]", "[data-tag3]", "[data-tag4]", "[data-tag5]" ]
+    fields.forEach(field => {
+      editCard.querySelector(field).value = "";
+    });
+  }
+};
 
 searchInput.addEventListener("input", (e) => {
   var last;
+  EditCardHandler.hide();
   while (last = acronymCardContainer.lastChild) {
     acronymCardContainer.removeChild(last);
   }
 
-  fetchAndShow(URL + e.target.value)
+  fetch(SEARCH_URL + e.target.value)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(acronym => {
+        const card = acronymCardTemplate.content.cloneNode(true).children[0];
+        AcronymCardHandler.assign(card, acronym);
+        acronymCardContainer.append(card);
+      })
+    })
 
   /*
   const value = e.target.value.toLowerCase()
@@ -80,4 +237,7 @@ searchInput.addEventListener("input", (e) => {
   */
 })
 
-fetchAndShow(URL)
+document.querySelector("[data-add]").addEventListener("click", function () { EditCardHandler.toggle(); });
+EditCardHandler.init();
+refreshTags();
+triggerSearch("")
--- index.php
+++ index.php
@@ -1,112 +1,262 @@
 <?php
 
-/* Path to SQLite database file */
-const DB_FILENAME = "acronyms.db";
-
 /* Path to index.html file */
 const INDEX_HTML = "index.html";
 
 ini_set("display_errors", true);
 
 class AcronymsDB extends SQLite3 {
-       function __construct($filename=DB_FILENAME) {
-              parent::__construct($filename);
+    function __construct($filename) {
+        parent::__construct($filename);
        
-              $create =<<<EOF
+        $create =<<<EOF
               CREATE TABLE IF NOT EXISTS acronyms (
-                id INT PRIMARY KEY NOT NULL,
+                id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
-                expanded TEXT NOT NULL,
+                definition TEXT NOT NULL,
                 tags TEXT,
                 description TEXT,
                 link TEXT,
-                created INT NOT NULL,
-                updated INT NOT NULL
+                created DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
+                updated DATE DEFAULT NULL,
+                deleted DATE DEFAULT NULL
               );
-              CREATE TABLE IF NOT EXISTS tags (
-                id INT PRIMARY KEY NOT NULL,
-                name TEXT NOT NULL
-              );
-              EOF;
+        EOF;
               
-              $result = parent::exec($create);
-              if (!$result) {
-                     parent::close();
-                     throw new Exception("Unable to create database schema: " + $db->lastErrorMsg());
-              }
-       }
+        $result = parent::exec($create);
+        if (!$result) {
+            parent::close();
+            throw new Exception("Unable to create database schema: " + $this->lastErrorMsg());
+        }
+    }
 
-       function __desctruct() {
-              parent::close();
-       }
+    function __desctruct() {
+        parent::close();
+    }
+
+    function getAcronyms($pattern="", $limit=0) {
+        $pattern = AcronymsDB::escapeString($pattern);
+        $sql = "SELECT DISTINCT id,name,definition,description,tags,link FROM acronyms " .
+               "WHERE deleted is NULL AND " .
+               "      (name LIKE '%$pattern%' OR tags LIKE '%$pattern%' OR definition LIKE '%$pattern%' OR description LIKE '%$pattern%')" .
+               "ORDER BY name,definition";
+
+        if ($limit > 0) {
+            $sql .= " LIMIT " . (string)$limit . ";";
+        } else {
+            $sql .= ";";
+        }
+        error_log($sql);
+
+        $matches = array();
+        $result = $this->query($sql);
+        if ($result) {
+            while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                if ($row["tags"]) {
+                    $row["tags"] = explode(",", $row["tags"]);
+                }
+
+                array_push($matches, $row);
+            }
+        }
+
+        return json_encode($matches, JSON_PRETTY_PRINT);
+    }
+
+    function getTags($pattern="", $limit=0) {
+
+        $pattern = AcronymsDB::escapeString($pattern);
+        $sql = "WITH RECURSIVE split(value, str) AS (" .
+               "  SELECT NULL,tags||',' FROM acronyms WHERE tags IS NOT NULL" .
+               "  UNION ALL " .
+               "  SELECT" .
+               "    substr(str, 0, instr(str, ','))," .
+               "    substr(str, instr(str, ',')+1)" .
+               "    FROM split WHERE str!=''" .
+               ") SELECT DISTINCT value FROM split WHERE value LIKE '$pattern%';";
+
+        $matches = array();
+        $result = $this->query($sql);
+        if ($result) {
+            while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                array_push($matches, $row['value']);
+            }
+        }
+
+        return json_encode($matches, JSON_PRETTY_PRINT);
+    }
+
+    function addAcronym($data) {
+        $required = [ "name", "definition" ];
+        $optional = [ "description", "link", "tags" ];
+
+        $values = array();
+        foreach (array_merge($required, $optional) as &$key) {
+            if (in_array($key, array_keys($data))) {
+                if (is_array($data[$key])) {
+                    $values[":".$key] = implode(",", $data[$key]);
+                } else {
+                    $values[":".$key] = $data[$key];
+                }
+            } else if (in_array($key, $required)) {
+                return formatJsonStatus("Missing required key: " . $key);
+            }
+        }
+
+        $names1 = implode(",", str_replace(":", "", array_keys($values)));
+        $names2 = implode(",", array_keys($values));
+        $sql = "INSERT INTO acronyms (" . $names1 . ") VALUES (" . $names2 . ");";
+
+        $stmt = $this->prepare($sql);
+        if ($stmt === false) {
+            return formatJsonStatus("Failed to prepare SQL statement");
+        }
+        foreach($values as $key => $value) {
+            $stmt->bindValue($key, $value);
+        }
+        if ($stmt->execute() === false) {
+            return formatJsonStatus("Failed to add new acronym");
+        }
+
+        return formatJsonStatus();
+    }
+
+    function updateAcronym($id, $data) {
+        if (!is_numeric($id)) {
+            return formatJsonStatus("Invalid acronym id");
+        }
+        $id = intval($id);
+
+        $required = [ "name", "definition" ];
+        $optional = [ "description", "link", "tags" ];
+
+        $values = array();
+        $sets = "";
+        foreach (array_merge($required, $optional) as &$key) {
+            if (in_array($key, array_keys($data))) {
+                if (is_array($data[$key])) {
+                    $values[":".$key] = implode(",", $data[$key]);
+                } else {
+                    $values[":".$key] = $data[$key];
+                }
+                $sets .= "$key=:$key,";
+            } else if (in_array($key, $required)) {
+                return formatJsonStatus("Missing required key: " . $key);
+            }
+        }
+        $sets .= "updated=datetime('now', 'localtime')";
+
+        $sql = "UPDATE acronyms SET $sets WHERE id=:id;";
+        $stmt = $this->prepare($sql);
+        if ($stmt === false) {
+            return formatJsonStatus("Failed to prepare SQL statement");
+        }
+        foreach($values as $key => $value) {
+            $stmt->bindValue($key, $value);
+        }
+        $stmt->bindValue(":id", $id);
+        if ($stmt->execute() === false) {
+            return formatJsonStatus("Failed to add new acronym");
+        }
+
+        return formatJsonStatus();
+    }
+
+    function removeAcronym($id) {
+        if (!is_numeric($id)) {
+            return formatJsonStatus("Invalid acronym id");
+        }
+        $id = intval($id);
+
+        $sql = "UPDATE acronyms SET deleted=datetime('now', 'localtime') WHERE id=:id";
+        $stmt = $this->prepare($sql);
+        if ($stmt === false) {
+            return formatJsonStatus("Failed to prepare SQL statement");
+        }
+        $stmt->bindValue(":id", $id, SQLITE3_INTEGER);
+        $result = $stmt->execute();
+        if ($stmt->execute() === false) {
+            return formatJsonStatus("Failed to remove acronym");
+        //} else if ($stmt->rowCount() == 0) {
+          //return formatJsonStatus("No such acronym");
+        }
+
+        return formatJsonStatus();
+    }
 }
 
-function searchAcronyms($pattern, $limit) {
-       try {
-              $db = new AcronymsDB();
-       } catch (Exception $e) {
-              exit($e);
-       }
-       
-       // User is most likely looking for a specific acronym, why bother processing thousands records
-       if ($limit > 0) {
-              $sql_limit = " LIMIT " . (string)$limit;
-       } else {
-              $sql_limit = "";
-       }
-
-       if (strlen($pattern) == 0) {
-              $sqls = array(
-                     "SELECT * FROM acronyms ORDER BY name" . $sql_limit . ";",
-              );
-       } else {
-              $pattern = AcronymsDB::escapeString($pattern);
-              $sqls = array(
-                     "SELECT * FROM acronyms WHERE name LIKE '%$pattern%'" . $sql_limit . ";",
-                     "SELECT * FROM acronyms WHERE tags LIKE '%$pattern%'" . $sql_limit . ";",
-                     "SELECT * FROM acronyms WHERE definition LIKE '%$pattern%'" . $sql_limit . ";",
-                     "SELECT * FROM acronyms WHERE description LIKE '%$pattern%'" . $sql_limit . ";",
-              );
-       }
-       
-       $ids = array();
-       $matches = array();
-       foreach ($sqls as &$sql) {
-              $result = $db->query($sql);
-              if ($result) {
-                     while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-                            if ($limit > 0 && count($ids) >= $limit) {
-                                   break;
-                            }
-              
-                            if (!in_array($row["id"], $ids)) {
-                                   $row["tags"] = explode(",", $row["tags"]);
-       
-                                   array_push($ids, $row["id"]);
-                                   array_push($matches, $row);
-                            }
-                     }
-              }
-              if ($limit > 0 && count($ids) >= $limit) {
-                     break;
-              }
-       }
-       return json_encode($matches, JSON_PRETTY_PRINT);
+function formatJsonStatus($error="") {
+    if ($error) {
+        $retval = array(
+            "status" => "error",
+            "explanation" => $error
+        );
+    } else {
+        $retval = array(
+            "status" => "ok"
+        );
+    }
+    return json_encode($retval, JSON_PRETTY_PRINT);
 }
 
-$search = "";
-if (in_array("search", array_keys($_REQUEST))) {
-       if (in_array("limit", array_keys($_REQUEST))) {
-              $limit = intval($_REQUEST["limit"]);
-       } else {
-              $limit = 0;
-       }
-       header("Content-Type: application/json; charset=UTF-8");
-       echo searchAcronyms($_REQUEST["search"], $limit);
+$config = json_decode(file_get_contents("config.json"), false);
+if (!$config) {
+    echo formatJsonStatus("Missing or bad config.json file");
+    exit(0);
+}
+
+if ($config->datasource == "sqlite") {
+    try {
+        $db = new AcronymsDB($config->dbpath);
+    } catch (Exception $e) {
+        echo formatJsonStatus("Can't connect to database");
+        exit(0);
+    }
 } else {
-       header("Content-Type: text/html; charset=UTF-8");
-       $fp = fopen(INDEX_HTML, "rb");
-       fpassthru($fp);
+    echo formatJsonStatus("Invalid data source");
+    exit(0);
+}
+
+if (in_array("search", array_keys($_GET))) {
+    $pattern = $_GET["search"];
+    if (in_array("limit", array_keys($_REQUEST))) {
+        $limit = intval($_REQUEST["limit"]);
+    } else {
+        $limit = 0;
+    }
+
+    header("Content-Type: application/json; charset=UTF-8");
+    echo $db->getAcronyms($pattern, $limit);
+
+} else if (in_array("add", array_keys($_GET))) {
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    header("Content-Type: application/json; charset=UTF-8");
+    echo $db->addAcronym($data);
+
+} else if (in_array("update", array_keys($_GET))) {
+    $id = $_GET["update"];
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    header("Content-Type: application/json; charset=UTF-8");
+    echo $db->updateAcronym($id, $data);
+
+} else if (in_array("remove", array_keys($_GET))) {
+    $id = $_GET["remove"];
+
+    header("Content-Type: application/json; charset=UTF-8");
+    echo $db->removeAcronym($id);
+
+} else if (in_array("tags", array_keys($_GET))) {
+    $pattern = $_GET["tags"];
+
+    header("Content-Type: application/json; charset=UTF-8");
+    echo $db->getTags($pattern);
+
+} else {
+    header("Content-Type: text/html; charset=UTF-8");
+    $fp = fopen(INDEX_HTML, "rb");
+    fpassthru($fp);
 }
 
 ?>
